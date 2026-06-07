e51fe8c441a02c898597db4d3665b6ece4032af1
llGiovannall <laqi775@gmail.com>
2026-06-07 00:45:23 -0300

    feat: create registerpage

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import RegisterForm from "../../components/registerForm";
+export default function RegisterPage() {
+    return (
+     <div className="flex items-center justify-center h-screen">
+       
+      <RegisterForm />
+    </div>
+  );}
