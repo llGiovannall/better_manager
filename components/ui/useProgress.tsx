be266dd40a07488c83cb1408c5cd9df54d044e17
llGiovannall <laqi775@gmail.com>
2026-06-06 22:52:21 -0300

    feat: change loading bar logic

--- components/ui/useProgress.tsx
+++ components/ui/useProgress.tsx
@@ -1,25 +1,22 @@
 import React from "react";
-import { useRouter } from "next/navigation";
 
     
     
     
     export default function useProgress() {
     const [progress, setProgress] = React.useState(0);
-    const router = useRouter();
 React.useEffect(() => {
     const timer = setInterval(() => {
       setProgress((oldProgress) => {
         if (oldProgress === 100) {
-          return 100;
+           clearInterval(timer);
+           return 100;
         }
-        const diff = Math.random() * 2;
-        return Math.min(oldProgress + diff, 100);
+        return oldProgress + 1;
       });
-    if (progress === 100) {
-        router.push("/profile");
-      }
-    }, 1000);
+    }, 30);
+
+
     return () => {
       clearInterval(timer);
     };
